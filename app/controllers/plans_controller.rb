f27a458d4e9c9dcfb358aae1bb2c4b7587e66069
Benjamin Steephenson <BenjaminSteephenson@gmail.com>
2014-12-14 22:18:49 -0500

    allow CORS

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,8 @@
 class PlansController < ApplicationController
 
+	#Allows browsers on other domans to access this
+	before_action :set_headers
+
 	#Returns a JSON list of plans in a certain state and rating area
 	def search
 		@state = params[:state]
@@ -11,5 +14,12 @@ class PlansController < ApplicationController
 	def details
 		@id = params[:id]
 		render json: Plan.find(@id)
+	end
+
+	def set_headers
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+		headers['Access-Control-Request-Method'] = '*'
+		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 	end
 end
